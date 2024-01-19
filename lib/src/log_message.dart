6e155b2f3e14c6d4eb39d319eec4cde6cdc5a3f5
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2024-01-19 15:05:24 +0100

    refactor

--- lib/src/log_message.dart
+++ lib/src/log_message.dart
@@ -6,12 +6,6 @@ import 'logger.dart';
 /// In addition to just the message, it also contains information of which class
 /// or package has generated this message
 class LogMessage {
-  final LogLevel level;
-  final String message;
-  final DateTime timestamp;
-  final Logger logger;
-  final String? stackTrace;
-
   const LogMessage(
     this.level,
     this.message,
@@ -20,16 +14,27 @@ class LogMessage {
     this.logger,
   );
 
-  @override
-  int get hashCode =>
-      Object.hash(level, message, timestamp, logger, stackTrace);
+  final LogLevel level;
+  final String message;
+  final DateTime timestamp;
+  final Logger logger;
+  final String? stackTrace;
 
   @override
-  bool operator ==(Object o) =>
-      o is LogMessage &&
-      o.level == level &&
-      o.message == message &&
-      o.timestamp == timestamp &&
-      o.logger == logger &&
-      o.stackTrace == stackTrace;
+  int get hashCode => Object.hash(
+        level,
+        message,
+        timestamp,
+        logger,
+        stackTrace,
+      );
+
+  @override
+  bool operator ==(Object other) =>
+      other is LogMessage &&
+      other.level == level &&
+      other.message == message &&
+      other.timestamp == timestamp &&
+      other.logger == logger &&
+      other.stackTrace == stackTrace;
 }
